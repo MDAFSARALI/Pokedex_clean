24dab7efed7f02033edbf4dab1b188004429dc4c
MDAFSARALI <generaluserknown@gmail.com>
2024-08-31 14:49:03 +0530

    Done

--- src/components/Pokedex/Pokedex.jsx
+++ src/components/Pokedex/Pokedex.jsx
@@ -1,6 +1,6 @@
 import Search from "../Search/Search";
 import PokedexList from '../PokemonList/PokemonList'
-import '../Pokedex/pokedex.css'
+import './Pokedex/pokedex.css'
 function Pokedex(){
     return(
         <div className="pokedex-wrapper">
